4880b7710f8d8bea4d2d68bff2bf975ce8875fca
Samuel Noye <samuel.noye@amalitech.org>
2021-12-06 19:22:35 +0000

    Initial Commit

---
+++ Shared/Restart_App_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_App_01App.swift
+//  Shared
+//
+//  Created by Samuel Noye on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Restart_App_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
