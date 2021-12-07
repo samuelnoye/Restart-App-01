5622a3b30b2adb91b544e4fb4988f577270e2e33
Samuel Noye <samuel.noye@amalitech.org>
2021-12-07 11:13:48 +0000

    Update OnboardingView.swift

--- Shared/Screens/OnboardingView.swift
+++ Shared/Screens/OnboardingView.swift
@@ -54,8 +54,54 @@ struct OnboardingView: View {
                         .scaledToFit()
                 }
                 Spacer()
+                
                 //MARK:- FOOTER
-
+                ZStack{
+                    //1. BACKGROUND
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                   
+                    //2. CALL-TO-ACTION
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    
+                    //3. CAPSULE
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    //4. CIRCLE
+                    HStack{
+                      ZStack{
+                        Circle()
+                            .fill(Color("ColorRed"))
+                        Circle()
+                            .fill(.black.opacity(0.15))
+                            .padding(8)
+                        Image(systemName:"chevron.right.2")
+                            .font(.system(size: 24, weight:  .bold))
+                    }.foregroundColor(.white)
+                            .frame(width:80, height:80, alignment: .center)
+                            .onTapGesture {
+                            isOnboardingViewActive = false
+                            }
+                        Spacer()
+                    }
+                    
+                }
+                    .frame(height: 80, alignment: .center)
+                    .padding()
+                    
             }//: VStack
         }//: ZStack
     }
